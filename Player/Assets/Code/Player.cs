60c9a0dcc4bdaafa1802d0d2ca79df72457ad2c3
dsgonzag <45009733+dsgonzag@users.noreply.github.com>
2019-06-11 14:06:41 -0500

    version3.0: Añadiendo la ultima clase

--- Player/Assets/Code/Player.cs
+++ Player/Assets/Code/Player.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform trans;
     public Rigidbody2D body;
-    public float jumSpeed;
+
     public float walkingSpeed;
-    
+    public float jumpSpeed;
+
+    private void Awake()
+    {
+        trans = this.transform;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-     }
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
-        {   // x-axis movement
-            body.velocity += jumSpeed * Vector2.up;
+        { // x-axis movement
+            body.velocity += jumpSpeed * Vector2.up;
         }
-        {   // x-axis movement
+
+        { // x-axis movement
             var v = body.velocity;
             var speed = 0f;
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                speed= -walkingSpeed;
+                speed += -walkingSpeed;
             }
-
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 speed += walkingSpeed;
@@ -36,8 +44,17 @@ public class Player : MonoBehaviour
             body.velocity = v;
         }
     }
-}
-        
-       
-    
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var otherObject = collision.collider.gameObject;
+        if (otherObject.tag == "Magnifier")
+        {
+            var scale = this.transform.localScale;
+            scale.y *= 2;
+            this.transform.localScale = scale;
+            //otherObject.SetActive(false);
+            //GameObject.Destroy(otherObject);
+        }
+    }
+}
